c1681784fb5d3481e007da65014afbc6658eb370
Kevin Yoon <KevinY105@gmail.com>
2015-05-11 21:19:47 -0500

    first test added and Qunit working

--- javascript/accordion/tests.js
+++ javascript/accordion/tests.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
   module("Accordion tests");
 
-  test("example test", function() {
-    ok(true);
+  test("all hidden at start test", function(assert) {
+    var hiddenElementsCount = $(".accordion-header div:hidden").length;
+    assert.equal(hiddenElementsCount, 5, "We expect all 5 subject div elements to be hidden");
   });
 
 });
