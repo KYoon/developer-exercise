4bc4e66133b7e526143384e54893a224c3e8758e
Kevin Yoon <KevinY105@gmail.com>
2015-05-11 21:33:44 -0500

    add three tests

--- javascript/accordion/tests.js
+++ javascript/accordion/tests.js
@@ -6,4 +6,19 @@ $(document).ready(function(){
     assert.equal(hiddenElementsCount, 5, "We expect all 5 subject div elements to be hidden");
   });
 
+  test("accordion test", function(assert) {
+    var mathAnchor = $("a:contains('Math')");
+    mathAnchor.click();
+    ok(mathAnchor.siblings().is(":visible"), "We expect the Math div to be shown when clicked on");
+  });
+
+  test("accordion test 2", function(assert) {
+    var mathAnchor = $("a:contains('Math')");
+    var englishAnchor = $("a:contains('English')");
+    mathAnchor.click();
+    englishAnchor.click();
+    notOk(mathAnchor.siblings().is(":hidden"), "We expect the Math div to be hidde when English is clicked");
+    ok(englishAnchor.siblings().is(":visible"), "We expect the English div to be shown when clicked on");
+  });
+
 });
